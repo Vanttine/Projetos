6ad82b8abd42d5b48941b395a3560f70706bf9d4
Luiz <fellipevantine12@gmail.com>
2025-11-27 18:21:44 -0300

    CryptoCoins

--- CryptoCoin/src/containers/App/App.jsx
+++ CryptoCoin/src/containers/App/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
       {/* container full */}
       <div className="container h-100 d-flex flex-column justify-content-center align-items-center text-center text-lg-start">
-        <div className="row w-100 text-white">
+        <div className=" row w-100 text-white">
           <div className="col-12 col-lg-6 mb-5 mb-lg-0 d-flex flex-column align-items-center align-items-lg-start">
             <h1 className="fw-bold">CryptoCoins Dashboard</h1>
             <p className="fs-5">
@@ -17,7 +17,10 @@ function App() {
           </div>
 
           <div className="col-12 col-lg-5 offset-lg-1 d-flex justify-content-center">
-            <form className="p-4 rounded-4 w-100" style={{ maxWidth: "400px" }}>
+            <form
+              className="p-4 glass-card w-100"
+              style={{ maxWidth: "400px" }}
+            >
               <div className="mb-3">
                 <label className="form-label">Email</label>
                 <input type="email" className="form-control" />
@@ -32,12 +35,17 @@ function App() {
                 <input type="checkbox" className="form-check-input" />
                 <label className="form-check-label ms-1">Lembrar de mim</label>
 
-                <a href="" className="ms-auto">
-                  Não tem uma conta?
+                <a
+                  href=""
+                  className="ms-auto border-bottom link-underline-light link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                >
+                  Não tenho conta.
                 </a>
               </div>
 
-              <button className="btn btn-primary w-100">Continuar</button>
+              <button className="btn btn-outline-secondary w-100">
+                Continuar
+              </button>
             </form>
           </div>
         </div>
