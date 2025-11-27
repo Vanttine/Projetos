4ea065f38404e0d4b94ff1171810e5afb4c81fac
Luiz <fellipevantine12@gmail.com>
2025-11-27 13:29:13 -0300

    Projeto nutricionista

---
+++ Nutri/script.js
@@ -0,0 +1,7 @@
+function autoGrow(el) {
+  el.style.height = "auto";
+  el.style.height = el.scrollHeight + "px";
+}
+
+const select = document.getElementById("objetivoSelect");
+const outrosCampo = document.getElementById("outrosCampo");
